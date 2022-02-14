7739a67f8bc5a9fe5ddf97c0613ab9b73b62b171
Fabian Arrotin <arrfab@centos.org>
2022-02-14 15:02:29 +0100

    Testing underlying eth device if we're using a bridge
    Signed-off-by: Fabian Arrotin <arrfab@centos.org>

--- files/scripts/zabbix-check-eth-settings.sh
+++ files/scripts/zabbix-check-eth-settings.sh
@@ -2,9 +2,13 @@
 export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin
 uname -a|grep -q xen >/dev/null
 if [ "$?" -eq "0" ] ;then
- eth_dev=p$(ip route|grep default|awk '{print $5}')
+  eth_dev=p$(ip route|grep default|awk '{print $5}')
 else
- eth_dev=$(ip route|grep default|awk '{print $5}')
+  eth_dev=$(ip route|grep default|awk '{print $5}')
+  # Specific case testing if bridge interface
+  if [[ "$eth_dev" == br* ]] ; then  
+    eth_dev=$(brctl show $eth_dev|tail -n 1|awk '{print $4}')
+  fi
 fi
 
 lsmod |grep -q -E 'virtio_net|xen_net'
@@ -12,7 +16,5 @@ if [ "$?" -eq "1" ] ;then
   zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -k eth_dev.speed -o $(ethtool $eth_dev|grep Speed|awk '{print $2}'|tr -d [:alpha:]|tr -d '/') >/dev/null
   zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -k eth_dev.duplex -o $(ethtool $eth_dev|grep Duplex|awk '{print $2}') >/dev/null
 fi
-
-
 
 
