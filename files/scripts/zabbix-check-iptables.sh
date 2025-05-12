d190d9860aa13d4d795e1907553c8a647e80d7ac
Fabian Arrotin <arrfab@centos.org>
2025-05-12 15:47:01 +0200

    Supporting nft instead of iptables for el10+
    Signed-off-by: Fabian Arrotin <arrfab@centos.org>

--- files/scripts/zabbix-check-iptables.sh
+++ files/scripts/zabbix-check-iptables.sh
@@ -2,10 +2,20 @@
 # called by Zabbix to see if iptables is running
 PATH=$PATH:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin
 
-if [ $( iptables -n -L INPUT|wc -l ) -gt 6 ] ; then
-  iptablesstatus="0"
-else
-  iptablesstatus="1"
-fi
+os_release=$(rpm -qf /etc/redhat-release --queryformat '%{Version}'|cut -f 1 -d '.')
 
+if [ ${os_release} -gt 9 ] ; then
+  if [ $(nft list chain inet filter INPUT|wc -l) -gt 6 ] ; then
+    iptablesstatus="0"
+  else
+    iptablesstatus="1"
+  fi
+else
+  if [ $( iptables -n -L INPUT|wc -l ) -gt 6 ] ; then
+    iptablesstatus="0"
+  else
+    iptablesstatus="1"
+  fi
+fi
 zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -k net.iptables.status -o $iptablesstatus >/dev/null
+
